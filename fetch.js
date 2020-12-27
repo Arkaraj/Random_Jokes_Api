26407b47aa6a74bb547ba66358ba1b3da69f1423
Arkaraj Ghosh <55324916+Arkaraj@users.noreply.github.com>
2020-12-27 21:41:01 +0530

    ⚙️Added Gifs

--- fetch.js
+++ fetch.js
@@ -8,6 +8,11 @@ let Author = document.querySelector('#name');
 let load1 = document.querySelector('#load1');
 let load2 = document.querySelector('#load2');
 let load3 = document.querySelector('#load3');
+let load4 = document.querySelector('#load4');
+let searchQuery = document.querySelector('#search');
+let gifSearch = document.querySelector('#gifsearch');
+let giffer = document.querySelector('#giffer');
+
 let source = 'ajax-loader.gif';
 
 // https://api.chucknorris.io/jokes/random
@@ -28,9 +33,12 @@ async function generateJoke() {
     const normalJoke = await fetch("https://dad-jokes.p.rapidapi.com/random/joke", {
         "method": "GET",
         "headers": {
-            "x-rapidapi-key": "ce58f459a0mshf0196057e588143p1221f4jsn5b83bf4c82fc",
+            "x-rapidapi-key": keys.jokes,
             "x-rapidapi-host": "dad-jokes.p.rapidapi.com"
         }
+    }).catch((err) => {
+        console.log(err);
+        jokeNormal.innerHTML = "Sorry Api didn't work";
     });
     const njoke = await normalJoke.json();
     jokeNormal.innerHTML = njoke.body[0].setup + '<br>' + njoke.body[0].punchline;
@@ -42,7 +50,7 @@ async function generateQuote() {
     const getquote = await fetch("https://quotes15.p.rapidapi.com/quotes/random/", {
         "method": "GET",
         "headers": {
-            "x-rapidapi-key": "ce58f459a0mshf0196057e588143p1221f4jsn5b83bf4c82fc",
+            "x-rapidapi-key": keys.jokes,
             "x-rapidapi-host": "quotes15.p.rapidapi.com"
         }
     });
@@ -51,6 +59,21 @@ async function generateQuote() {
     quotePara.innerHTML = `"${quote.content}"`;
     Author.innerHTML = `--${quote.originator.name}`;
 }
+
+async function generateGif() {
+
+    const val = (searchQuery.value).split(/\s/).join('+');
+    const getGif = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${keys.gifs}&tag=${val}&limit=1`);
+
+    const gif = await getGif.json();
+    load4.src = gif.data.images.original.url;
+    load4.style.display = 'block';
+    load4.style.margin = '0';
+    //giffer.innerHTML = gif.data[0].images.original.url;
+    console.log(gif.data[0].images.downsized.url)
+}
+
 jokegen.addEventListener('click', generateChuckJoke);
 jokeNormalButton.addEventListener('click', generateJoke);
 quoteButton.addEventListener('click', generateQuote);
+gifSearch.addEventListener('click', generateGif);
